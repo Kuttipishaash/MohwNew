c4cfe68eb432f651659f92509996da0f5d6067b1
Prejith Premkumar <prajithpremg@gmail.com>
2018-03-30 12:38:48 +0530

    implemented report adding functionality

--- app/src/main/java/com/hackit/mohwnew/AddReportActivity.java
+++ app/src/main/java/com/hackit/mohwnew/AddReportActivity.java
@@ -1,13 +1,55 @@
 package com.hackit.mohwnew;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddReportActivity extends AppCompatActivity {
+
+    EditText mTitleField;
+    EditText mProgramNameField;
+    EditText mDescField;
+    Button mSubmitButton;
+
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_report);
+
+        mTitleField = (EditText) findViewById(R.id.et_report_title);
+        mDescField = (EditText) findViewById(R.id.et_report_description);
+        mProgramNameField = (EditText) findViewById(R.id.et_report_program);
+        mSubmitButton = (Button) findViewById(R.id.btn_report_submit);
+
+        mTitleField.requestFocus();
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Report report = new Report();
+                report.setTitle(mTitleField.getText().toString());
+                report.setDesc(mDescField.getText().toString());
+                report.setProgName(mProgramNameField.getText().toString());
+                databaseReference.child("reports").push().setValue(report);
+
+                Toast.makeText(AddReportActivity.this, "Report created", Toast.LENGTH_SHORT).show();
+
+                mTitleField.setText("");
+                mDescField.setText("");
+                mProgramNameField.setText("");
+                mTitleField.requestFocus();
+            }
+        });
     }
 }
