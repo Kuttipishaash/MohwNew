704a699c774ce887007c0c96956f808cd30940ca
Prejith Premkumar <prajithpremg@gmail.com>
2018-03-31 03:18:20 +0530

    create activity to view programs

--- app/src/main/java/com/hackit/mohwnew/ProgramsViewActivity.java
+++ app/src/main/java/com/hackit/mohwnew/ProgramsViewActivity.java
@@ -2,12 +2,24 @@ package com.hackit.mohwnew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
 
 public class ProgramsViewActivity extends AppCompatActivity {
+
+    DatabaseReference databaseReference;
+    List<Program> programList;
+    ListView mProgramsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_programs_view);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("programs");
     }
 }
