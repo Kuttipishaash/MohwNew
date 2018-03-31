9840dab8d949916dd791d8cd15558a331f314658
Prejith Premkumar <prajithpremg@gmail.com>
2018-03-31 08:38:45 +0530

    update program viewing implementation

--- app/src/main/java/com/hackit/mohwnew/ProgramStateViewActivity.java
+++ app/src/main/java/com/hackit/mohwnew/ProgramStateViewActivity.java
@@ -2,12 +2,79 @@ package com.hackit.mohwnew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
 
 public class ProgramStateViewActivity extends AppCompatActivity {
+
+    String title;
+    String completed;
+    String completion_rate;
+    String description;
+    String total;
+    String under;
+    String id;
+
+    DatabaseReference programRef;
+    DatabaseReference stateRef;
+
+    List<String> districtList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_program_state_view);
+
+        title = getIntent().getStringExtra("title");
+        id = getIntent().getStringExtra("id");
+
+        getSupportActionBar().setTitle(title);
+
+    /*    completed = getIntent().getStringExtra("completed");
+        completion_rate = getIntent().getStringExtra("completion_rate");
+        description = getIntent().getStringExtra("description");
+        total = getIntent().getStringExtra("total");
+        under = getIntent().getStringExtra("under");
+*/
+
+        programRef = FirebaseDatabase.getInstance().getReference().child("programs");
+        stateRef = FirebaseDatabase.getInstance().getReference().child("states");
+
+        stateRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String district = dataSnapshot.getValue(String.class);
+                Toast.makeText(ProgramStateViewActivity.this, district + "", Toast.LENGTH_SHORT).show();
+                districtList.add(district);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/hackit/mohwnew/ProgramsViewActivity.java
+++ app/src/main/java/com/hackit/mohwnew/ProgramsViewActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -24,22 +25,27 @@ public class ProgramsViewActivity extends AppCompatActivity {
     ListView mProgramsList;
     ProgramsAdapter mProgramAdapter;
     ProgressBar mProgressBar;
+    List<String> programIds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_programs_view);
+        getSupportActionBar().setTitle("Programs");
 
         programList = new ArrayList<Program>();
+        programIds = new ArrayList<String>();
         mProgramsList = (ListView) findViewById(R.id.list_view_programs);
         mProgressBar = (ProgressBar) findViewById(R.id.progress_view_programs);
 
         databaseReference = FirebaseDatabase.getInstance().getReference().child("programs");
+        databaseReference.keepSynced(true);
         databaseReference.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Program program = dataSnapshot.getValue(Program.class);
                 programList.add(program);
+                programIds.add(dataSnapshot.getKey());
                 mProgramAdapter = new ProgramsAdapter(ProgramsViewActivity.this, R.layout.item_program, programList);
                 mProgramsList.setAdapter(mProgramAdapter);
                 mProgressBar.setVisibility(View.GONE);
@@ -73,12 +79,14 @@ public class ProgramsViewActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(getBaseContext(), ProgramStateViewActivity.class);
                 intent.putExtra("title", clickedProgram.getTitle());
-                intent.putExtra("completed", Integer.toString(clickedProgram.getCompleted()));
+                intent.putExtra("id",  programIds.get(position));
+                Toast.makeText(ProgramsViewActivity.this, programIds.get(position) + "", Toast.LENGTH_SHORT).show();
+/*                intent.putExtra("completed", Integer.toString(clickedProgram.getCompleted()));
                 intent.putExtra("completion_rate", Float.toString(clickedProgram.getCompletionRate()));
                 intent.putExtra("description", clickedProgram.getDescription());
                 intent.putExtra("total", Integer.toString(clickedProgram.getTotal()));
                 intent.putExtra("under", clickedProgram.getUnder());
-                startActivity(intent);
+*/                startActivity(intent);
             }
         });
     }
